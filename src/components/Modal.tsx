03cf71881bff876e67bddac552c9dc5d081e676d
nicolasQuack <nicolas.quack@icloud.com>
2024-03-02 20:47:03 -0300

    style(components/): add shandow and configure it;

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,6 @@
 export function Modal({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
-        <div className="bg-transparent p-8 rounded-2xl shadow-2xl space-y-4">
+        <div className="p-8 py-16 rounded-2xl space-y-4 border shadow-2xl dark:shadow-zinc-700">
             {children}
         </div>
     )
