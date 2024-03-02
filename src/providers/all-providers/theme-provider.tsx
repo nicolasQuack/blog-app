76b93c7132ef35e16391d362f5c2980622b48bbc
nicolasQuack <nicolas.quack@icloud.com>
2024-03-02 20:48:38 -0300

    feat(providers/all-providers/): configure theme provider;

--- src/providers/all-providers/theme-provider.tsx
+++ src/providers/all-providers/theme-provider.tsx
@@ -1,12 +1,15 @@
 "use client"
 
-import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export function ThemeProvider({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
-        <NextThemesProvider {...props}>
+        <NextThemesProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem={true}
+            themes={["light", "dark"]}
+        >
             {children}
         </NextThemesProvider>
     )
