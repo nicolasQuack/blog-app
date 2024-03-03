54d8334b45ddc496aca2d794d8a53972155e8b18
nicolasQuack <nicolas.quack@icloud.com>
2024-03-03 01:01:33 -0300

    feat((root)/signup/): add logo to layout;

--- src/app/(root)/signup/page.tsx
+++ src/app/(root)/signup/page.tsx
@@ -1,10 +1,12 @@
 import { SignUpForm, Modal, ModeToggle } from "@/components"
+import { LogoButton } from "@/components/LogoButton"
 
 export default function Page() {
     return (
-        <main className="">
-            <div className="absolute">
-                <div className="flex p-4 w-screen justify-end">
+        <main className="bg-background">
+            <div className="absolute ">
+                <div className="flex p-4 w-screen justify-between">
+                    <LogoButton href="/" />
                     <ModeToggle align="end" />
                 </div>
             </div>
