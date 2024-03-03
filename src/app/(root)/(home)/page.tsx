a6f54f6c943bbb011fc0894f8aae31787d7f8c67
nicolasQuack <nicolas.quack@icloud.com>
2024-03-03 20:36:35 -0300

    chore((root)/): move file to (home) folder;

---
+++ src/app/(root)/(home)/page.tsx
@@ -0,0 +1,11 @@
+import { HomeHeader, MainContent, Posts } from "./_component";
+
+export default function Home() {
+  return (
+    <main>
+      <HomeHeader />
+      <MainContent />
+      <Posts />
+    </main>
+  );
+}
