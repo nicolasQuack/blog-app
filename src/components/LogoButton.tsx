217b484aa19a127b0c8e992ed441f318373d41f8
nicolasQuack <nicolas.quack@icloud.com>
2024-03-03 03:47:25 -0300

    style(components/): add effect while tap and change size to remove padding;

--- src/components/LogoButton.tsx
+++ src/components/LogoButton.tsx
@@ -15,10 +15,10 @@ export function LogoButton({ href }: LogoButtonProps) {
     return (
         <motion.div
             whileHover={{ scale: 1.4, rotate: -15, transition: { duration: 0.3 } }}
-            whileTap={{ scale: 0.8 }}
+            whileTap={{ scale: 0.8, rotate: 15, transition: { duration: 0.3 } }}
         >
-            <Button variant={"link"} onClick={() => router.push(href)}>
-                <Image src="/images/logo.svg" alt="logo" width="36" height="36" className="pointer-events-none" />
+            <Button size={"icon"} variant={"link"} onClick={() => router.push(href)}>
+                <Image priority src="/images/logo.svg" alt="logo" width="36" height="36" className="pointer-events-none" />
             </Button>
         </ motion.div >
     )
