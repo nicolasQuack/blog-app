59e8b746c288b00cec47450e2dc1f0f1cef99be0
nicolasQuack <nicolas.quack@icloud.com>
2024-03-03 20:20:00 -0300

    style(/components): add convention for import components;

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,5 @@ export * from "./Modal";
 export * from "./Signup-Form";
 export * from "./ModeToggle";
 export * from "./LogoButton";
-export * from "./HomeHeader";
 export * from "./DefaultButton";
+export * from "./NavMenu";
