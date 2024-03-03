6ce40d6226b5db328264871e8dabe51dcd58acc2
nicolasQuack <nicolas.quack@icloud.com>
2024-03-03 01:35:44 -0300

    refactor((root)/signup/): transfer the header to the layout;

---
+++ src/app/(root)/signup/layout.tsx
@@ -0,0 +1,20 @@
+import { Modal, ModeToggle } from "@/components";
+import { LogoButton } from "@/components/LogoButton";
+
+export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <main>
+            <div className="absolute ">
+                <div className="flex p-4 w-screen justify-between">
+                    <LogoButton href="/" />
+                    <ModeToggle align="end" />
+                </div>
+            </div>
+            <div className="flex w-screen h-screen justify-center items-center">
+                <Modal>
+                    {children}
+                </Modal>
+            </div>
+        </main>
+    )
+}
--- src/app/(root)/signup/page.tsx
+++ src/app/(root)/signup/page.tsx
@@ -3,18 +3,8 @@ import { LogoButton } from "@/components/LogoButton"
 
 export default function Page() {
     return (
-        <main className="bg-background">
-            <div className="absolute ">
-                <div className="flex p-4 w-screen justify-between">
-                    <LogoButton href="/" />
-                    <ModeToggle align="end" />
-                </div>
-            </div>
-            <div className="flex w-screen h-screen justify-center items-center">
-                <Modal>
-                    <SignUpForm />
-                </Modal>
-            </div>
-        </main>
+        <>
+            <SignUpForm />
+        </>
     )
 }
