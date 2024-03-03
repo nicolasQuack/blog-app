37793dc18c8d03efd1ebfe498e83f744bcd1e300
nicolasQuack <nicolas.quack@icloud.com>
2024-03-03 03:51:33 -0300

    feat((root)/): configure and style home page;

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,30 @@
+import { HomeHeader } from "@/components";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen flex justify-center items-center">
-      <h1 className="text-3xl font-medium">Hello World</h1>
-    </div>
+    <main>
+      <div className="absolute">
+        <HomeHeader />
+      </div>
+      <div className="h-screen flex justify-center items-center">
+        <div className="flex space-x-6  justify-center select-none pointer-events-none">
+          <Image
+            src={"/images/template.svg"}
+            width={450}
+            height={450}
+            alt="template"
+            className=""
+          />
+          <div className="flex flex-col justify-center w-[450px] space-y-8">
+            <h1 className="text-7xl font-bold">
+              Think, Write and Share.
+            </h1>
+            <p className="text-lg font-normal">A web blog developed and designed to give you an enjoyable browsing experience.
+              by Nicolas Florencio Felipe.</p>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
