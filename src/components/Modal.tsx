2e36ebf6a936902f7bbc3adfa62243fd17056ac9
nicolasQuack <nicolas.quack@icloud.com>
2024-03-03 00:59:43 -0300

    style(components/): change modal border radius from '2xl' to 'xl';

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,6 @@
 export function Modal({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
-        <div className="p-8 py-16 rounded-2xl space-y-4 border shadow-2xl dark:shadow-zinc-700">
+        <div className="p-8 py-16 rounded-xl space-y-4 border shadow-2xl dark:shadow-zinc-700">
             {children}
         </div>
     )
