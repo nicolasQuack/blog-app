5368a93ee7dc2ef846637c74be39ae3bf9e0b623
nicolasQuack <nicolas.quack@icloud.com>
2024-03-03 20:34:53 -0300

    style(styles/): add padding and fixed position to component;

--- src/components/SignUpHeader.tsx
+++ src/components/SignUpHeader.tsx
@@ -4,11 +4,13 @@ import { DefaultButton, LogoButton, ModeToggle } from ".";
 
 export function SignUpHeader() {
     return (
-        <div className="flex p-4 w-screen justify-between">
-            <LogoButton href="/" />
-            <div className="flex space-x-4">
-                <DefaultButton href="/login" title="Log In" />
-                <ModeToggle align="end" />
+        <div className="fixed">
+            <div className="flex py-4 px-12 w-screen justify-between">
+                <LogoButton href="/" />
+                <div className="flex space-x-4">
+                    <DefaultButton href="/login" title="Log In" />
+                    <ModeToggle align="end" />
+                </div>
             </div>
         </div>
     )
