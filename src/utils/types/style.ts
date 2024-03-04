1e485a8a8df16e56b7de1f123bf73f7963938c75
nicolasQuack <nicolas.quack@icloud.com>
2024-03-04 01:32:52 -0300

    style(utils/types/): add types and convention for import it;

---
+++ src/utils/types/style.ts
@@ -0,0 +1,26 @@
+export type Shadows =
+  | "shadow-sm"
+  | "shadow-md"
+  | "shadow-lg"
+  | "shadow-xl"
+  | "shadow-2xl"
+  | "shadow-inner"
+  | "shadow-none";
+
+export type Rounded =
+  | "rounded-none"
+  | "rounded-sm"
+  | "rounded-md"
+  | "rounded-lg"
+  | "rounded-xl"
+  | "rounded-2xl"
+  | "rounded-3xl"
+  | "rounded-full";
+
+export type Variant =
+  | "default"
+  | "destructive"
+  | "outline"
+  | "secondary"
+  | "ghost"
+  | "link";
