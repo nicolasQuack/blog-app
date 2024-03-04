376e541462c5133671dde3347a861119e3826d23
nicolasQuack <nicolas.quack@icloud.com>
2024-03-04 01:36:44 -0300

    docs((root)/signup/): remove unused imports;

--- src/app/(root)/signup/layout.tsx
+++ src/app/(root)/signup/layout.tsx
@@ -1,7 +1,5 @@
-import { DefaultButton, HomeHeader, Modal, ModeToggle } from "@/components";
-import { LogoButton } from "@/components/LogoButton";
+import { Modal } from "@/components";
 import { SignUpHeader } from "@/components/SignUpHeader";
-import { Button } from "@/components/ui/button";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
