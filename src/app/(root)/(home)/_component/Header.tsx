f0f05d14481f89a39fe0f1b54cb112c9be570fc2
nicolasQuack <nicolas.quack@icloud.com>
2024-03-03 21:15:58 -0300

    fix((home)/_component/): active nav bar after and before hidden animation;

--- src/app/(root)/(home)/_component/Header.tsx
+++ src/app/(root)/(home)/_component/Header.tsx
@@ -7,34 +7,40 @@ export function HomeHeader() {
 
     if (typeof window !== "undefined") {
         window.addEventListener('scroll', () => {
-            const header = document.querySelector('div#scroll-down-hidden')
-            if (header) {
+            const right = document.querySelector('div#right-scroll-down-hidden')
+            const left = document.querySelector('div#left-scroll-down-hidden')
+
+            if (right && left) {
                 if (window.scrollY > 0) {
-                    header.classList.add('animate-hidden-up')
-                    header.classList.remove('animate-hidden-up-show')
+                    right.classList.add('animate-hidden-up')
+                    right.classList.remove('animate-hidden-up-show')
+                    left.classList.add('animate-hidden-up')
+                    left.classList.remove('animate-hidden-up-show')
                 }
                 else {
-                    header.classList.add('animate-hidden-up-show')
-                    header.classList.remove('animate-hidden-up')
+                    right.classList.add('animate-hidden-up-show')
+                    right.classList.remove('animate-hidden-up')
+                    left.classList.add('animate-hidden-up-show')
+                    left.classList.remove('animate-hidden-up')
                 }
             }
         })
     }
 
     return (
-        <header className="absolute max-h-full py-4 px-12 items-center w-screen justify-between">
-            <div className="flex justify-center items-start">
-                <div className="fixed ">
-                    <div className="flex col-span-2 items-center justify-center">
-                        <NavMenu />
-                    </div>
-                </div>
-            </div>
-            <div id="scroll-down-hidden" className="grid grid-cols-2 ">
-                <div className="flex">
+        <header className="absolute max-h-full py-4 px-12 items-center w-screen select-none justify-between">
+            <div className="grid grid-cols-4">
+                <div id="right-scroll-down-hidden" className="flex">
                     <LogoButton href="/" />
                 </div>
-                <div className="flex justify-end space-x-4">
+                <div className="flex col-span-2 justify-center items-star z-10">
+                    <div className="fixed ">
+                        <div className="flex items-center justify-center">
+                            <NavMenu />
+                        </div>
+                    </div>
+                </div>
+                <div id="left-scroll-down-hidden" className="flex justify-end space-x-4">
                     <DefaultButton href="/signup" title="Sign Up" />
                     <ModeToggle align="end" />
                 </div>
