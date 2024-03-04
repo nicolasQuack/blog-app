caa74d614425f2319c4c2a1cd71f100e313aa6e6
nicolasQuack <nicolas.quack@icloud.com>
2024-03-04 01:34:08 -0300

    feat(components/): add new props to modal component;

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,16 @@
-export function Modal({ children }: Readonly<{ children: React.ReactNode }>) {
+import { Rounded, Shadows } from "../utils/types/style"
+
+interface ModalProps {
+    children: React.ReactNode
+    shadow?: Shadows
+    padding?: string
+    rounded?: Rounded
+    className?: string
+}
+
+export function Modal({ children, shadow, padding, rounded, className }: Readonly<ModalProps>) {
     return (
-        <div className="p-8 py-16 rounded-xl space-y-4 border shadow-2xl dark:shadow-zinc-700">
+        <div className={`${className} ${shadow ? shadow : "shadow-2xl"} ${padding ? padding : "p-8 py-16"} ${rounded ? rounded : "rounded-xl"} space-y-4 border dark:shadow-zinc-700`}>
             {children}
         </div>
     )
