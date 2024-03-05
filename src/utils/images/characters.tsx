b32751d77d0a72602b376048e4d061b3abc2993c
nicolasQuack <nicolas.quack@icloud.com>
2024-03-05 20:08:34 -0300

    perf(utils/images/): change the svg according to the theme;

--- src/utils/images/characters.tsx
+++ src/utils/images/characters.tsx
@@ -4,41 +4,17 @@ import Image from "next/image";
 import DarkCharacter from "../../../public/images/character/dark.svg";
 import LightCharacter from "../../../public/images/character/light.svg";
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
 
 export function ThemedCharacter() {
-    const { theme, systemTheme } = useTheme();
-    const [currentTheme, setCurrentTheme] = useState(theme);
+    const { theme, resolvedTheme } = useTheme();
 
-    useEffect(() => {
-
-        if (theme === "system") {
-            if (systemTheme === "light") setCurrentTheme("light");
-
-            else setCurrentTheme("dark");
-
-        } else {
-            setCurrentTheme(theme);
-        }
-
-    }, [theme, systemTheme]);
-
-    return Character(currentTheme!);
-
-}
-
-export function Character(theme: string) {
-    return (
-        <div>
-            <Image
-                quality={100}
-                priority
-                src={theme === "light" ? LightCharacter : DarkCharacter}
-                className="h-auto w-80"
-                width={0}
-                height={0}
-                alt="character of not found"
-            />
-        </div>
-    );
+    return <Image
+        quality={100}
+        priority
+        src={theme === "system" ? (resolvedTheme === "dark" ? DarkCharacter : LightCharacter) : (theme === "dark" ? DarkCharacter : LightCharacter)}
+        className="h-auto w-80"
+        width={0}
+        height={0}
+        alt="character of not found"
+    />
 }
