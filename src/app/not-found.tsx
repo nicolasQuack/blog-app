76279b51a5f624c222b5d4d5a951a720a9a8711d
nicolasQuack <nicolas.quack@icloud.com>
2024-03-05 04:33:02 -0300

    feat(app/): add not found page with themed character;

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,23 @@
-export default function NotFound() {
+
+import { DefaultButton } from "@/components";
+import { ThemedCharacter } from "@/utils/images";
+
+export default function Page() {
+
     return (
-        <div>
-            <h1>Not Found</h1>
+        <div className="flex h-screen justify-center items-center">
+            <div className=" flex flex-col space-y-4 justify-center items-center">
+                <div className="h-full flex flex-col items-center select-none pointer-events-none space-y-2">
+                    <div className="h-auto border-b shadow-sm">
+                        <ThemedCharacter />
+                    </div>
+                    <h1 className="text-2xl font-bold">Page not found</h1>
+                    <p className="text-stone-500">we are probably working on it!</p>
+                </div>
+                <div className="flex items-center justify-center">
+                    <DefaultButton href="/" variant="destructive" title="Back to menu" />
+                </div>
+            </div>
         </div>
     )
 }
