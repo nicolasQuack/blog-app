2d5a154e49fc0d807e03d49d8faa05ca79529f64
nicolasQuack <nicolas.quack@icloud.com>
2024-03-07 04:35:07 -0300

    refactor(app/(home)): do responsive page;

--- src/app/(home)/_component/pages/Start.tsx
+++ src/app/(home)/_component/pages/Start.tsx
@@ -1,35 +1,34 @@
-'use client'
-
 import { DefaultButton } from "@/components";
 import Image from "next/image";
 
 export function Start() {
     return (
-        <>
-            <div className="h-screen flex flex-col justify-center items-center space-y-12">
-                <div className="flex space-x-12  justify-center select-none pointer-events-none">
-                    <Image
-                        src={"/images/template.svg"}
-                        width={450}
-                        height={450}
-                        alt="template"
-                        className=""
-                        priority
-                    />
-                    <div className="flex flex-col justify-center w-[450px] space-y-8">
-                        <h1 className="text-7xl font-bold">
-                            Think, Write and Share.
-                        </h1>
-                        <div className="text-lg font-normal">
-                            <p >A web blog developed and designed to give you an <span className="underline-offset-4 underline">enjoyable browsing experience</span>.</p>
-                            <p className="text-stone-500">by Nicolas Florencio Felipe.</p>
+        <div className="flex flex-col justify-center items-center border h-[900px] select-none">
+            <div className="flex space-x-5 h-1/2 lg:w-[900px]">
+                <div className="size-full">
+                    <Image className="pointer-events-none" src="/images/template.svg" alt="logo template" width={500} height={500} />
+                </div>
+                <div className="flex flex-col space-y-8 size-full justify-center">
+                    <div>
+                        <h1 className="text-6xl font-bold">Think, Write
+                            and Share.</h1>
+                    </div>
+                    <div className="text-lg font-normal">
+                        <div>
+                            <p>A web blog developed and designed to give you an
+                                enjoyable browsing experience.</p>
+                        </div>
+                        <div>
+                            <p className="text-muted-foreground">by Nicolas Florencio Felipe.</p>
                         </div>
                     </div>
                 </div>
-                <div>
-                    <DefaultButton href="/" title="Learn More About Project" className="py-7 px-14 text-white bg-blue-500" variant={"outline"} />
+            </div>
+            <div className="h-[100px] w-[900px]">
+                <div className="flex justify-center items-center w-full h-full">
+                    <DefaultButton href="/signup" title="Learn More About Project" className="py-7 px-14 text-white bg-blue-500" variant={"outline"} />
                 </div>
-            </div >
-        </>
+            </div>
+        </div>
     )
 }
