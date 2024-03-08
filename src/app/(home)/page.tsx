7800f012dddcfd9a970c9b07e011ab9dc4175e28
nicolasQuack <nicolas.quack@icloud.com>
2024-03-08 02:10:31 -0300

    feat(app/home/): add idea section and style it;

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,24 +2,16 @@ import { Header, Posts, Start, Creators, End, Idea, Tech } from "./_component";
 
 export default function Home() {
   return (
-    <main className="w-screen border flex justify-center">
+    <main className="w-screen flex justify-center">
       <div className="w-[2560px] flex flex-col justify-center">
         <div className="flex justify-center w-full">
           <div className="max-w-[2560px] w-full fixed">
             <Header />
           </div>
         </div>
-        <div className="h-fit w-full border">
+        <div className="h-fit w-full">
           <Start />
-          <Start />
-          <Start />
-          <Start />
-          <Start />
-          <Start />
-          <Start />
-          <Start />
-          <Start />
-          <Start />
+          <Idea />
         </div>
       </div>
     </main>
